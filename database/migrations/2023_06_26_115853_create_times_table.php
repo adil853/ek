0d9f075206f921a8e31f3ccccc8b519f58048994
Adil Abdullah <adilabdullah@LHR-Adil-Abdullah-MAC.local>
2023-06-26 16:59:43 +0500

    databse migration added

---
+++ database/migrations/2023_06_26_115853_create_times_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('times', function (Blueprint $table) {
+            $table->id();
+            $table->string('start_time')->nullable();
+            $table->string('end_time')->nullable();
+            $table->json('time_expressions')->nullable();
+            $table->timestamps();
+        });
+
+        // Add indexes for start_time and end_time
+        Schema::table('times', function (Blueprint $table) {
+            $table->index('start_time');
+            $table->index('end_time');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('times');
+    }
+};
