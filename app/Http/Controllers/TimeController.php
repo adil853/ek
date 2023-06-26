fc3fc858ea8cd5ec8e82336e1c2c7be18758e923
Adil Abdullah <adilabdullah@LHR-Adil-Abdullah-MAC.local>
2023-06-26 11:55:49 +0500

    router added with basic contrller

---
+++ app/Http/Controllers/TimeController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TimeController extends Controller
+{
+    public function breakTime(Request $request)
+    {
+        $inputs = [
+            'start_time' => $request->input('start_time'),
+            'end_time' => $request->input('end_time'),
+            'strings' => $request->input('time_expressions')
+        ];
+
+        $validator = validateInput($inputs);
+        if ($validator->fails()) {
+            $errors = $validator->errors();
+            return response()->json(['errors' => $errors], 420);
+        }
+        return response()->json(200);
+    }
+}
+
+
+function validateInput(array $data)
+{
+    return \Illuminate\Support\Facades\Validator::make($data, [
+        'start_time' => 'required|date_format:Y-m-d H:i:s',
+        'end_time' => 'required|date_format:Y-m-d H:i:s',
+        'strings' => [
+            'required',
+            'array',
+            function ($attribute, $value, $fail) {
+                foreach ($value as $item) {
+                    if (!preg_match('/^(\d+)?[mdhis]$/', $item)) {
+                        $fail("The $attribute array can only contain strings with the formats 'm', 'd', 'h', 'i', 's' or positive integers followed by 'm', 'd', 'h', 'i', 's'.");
+                        break;
+                    }
+                }
+            },
+        ],
+    ]);
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,17 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/breakTime', [\App\Http\Controllers\TimeController::class, 'breakTime']);
+
+
+
+
+
+
+
+
+
+
+
+
