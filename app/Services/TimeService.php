0430c86f28de040c61fcbb95f3f0961fcbcf0501
Adil Abdullah <adilabdullah@LHR-Adil-Abdullah-MAC.local>
2023-06-26 14:10:09 +0500

    basic implemented

--- app/Services/TimeService.php
+++ app/Services/TimeService.php
@@ -13,7 +13,16 @@ class TimeService
         $timeExpressions = $data["time_expressions"];
         $sortedExpressionTimeStrings = sortTimeStrings($timeExpressions);
 
+        $time1 = strtotime($startTime);
+        $time2 = strtotime($endTime);
 
+        $totalSeconds = $time2 - $time1;
+
+        $secondsInMonth = 2592000;
+        $secondsInDay = 86400;
+        $secondsInHour = 3600;
+        $secondsInMin = 60;
+        $secondsInSecond = 1;
 
         $alreadyValidExpressionExtracted["m"] = false;
         $alreadyValidExpressionExtracted["d"] = false;
@@ -24,13 +33,103 @@ class TimeService
         $count = 0;
 
         while ($count < count($sortedExpressionTimeStrings)) {
-            echo $sortedExpressionTimeStrings[$count];
+            $timeExpression = $sortedExpressionTimeStrings[$count];
+            preg_match('/^(\d+)([a-zA-Z]+)$/', $timeExpression, $match);
+            $quantity = $match[1];
+            $unit = $match[2];
+            if ($alreadyValidExpressionExtracted[$unit]) {
+                $count++;
+                continue;
+            }
+            if ($count == count($sortedExpressionTimeStrings) - 1) {
+
+                $secondsToDividedBy = 0;
+                switch ($unit) {
+                    case "m" :
+                        $secondsToDividedBy = $secondsInMonth;
+                        break;
+                    case "d" :
+                        $secondsToDividedBy = $secondsInDay;
+
+                        break;
+                    case "h" :
+                        $secondsToDividedBy = $secondsInHour;
+                        break;
+                    case "i" :
+                        $secondsToDividedBy = $secondsInMin;
+                        break;
+                    case "s" :
+                        $secondsToDividedBy = $secondsInSecond;
+                        break;
+                }
+                if ($secondsToDividedBy > 0) {
+                    $lastValue = $totalSeconds / ($secondsToDividedBy * $quantity);
+                    echo $lastValue . " unit of " . $quantity . " " . $unit . "___this is last one___";
+                }
+
+            } else {
+                switch ($unit) {
+                    case "m" :
+                        if (($totalSeconds / ($secondsInMonth * $quantity)) > 1) {
+                            $totalTimeInMonth = $totalSeconds / ($secondsInMonth * $quantity);
+                            $expressionQuantity = intval($totalTimeInMonth);
+                            $totalSeconds = ($totalTimeInMonth - $expressionQuantity) * $secondsInMonth;
+                            echo $expressionQuantity . "unit of " . $quantity . "m___";
+                            $alreadyValidExpressionExtracted["m"] = true;
+                        } else {
+                            echo "0 unit of " . $quantity . "m___";
+                        }
+                        break;
+                    case "d" :
+                        if (($totalSeconds / ($secondsInDay * $quantity)) > 1) {
+                            $totalTimeInDay = $totalSeconds / ($secondsInDay * $quantity);
+                            $expressionQuantity = intval($totalTimeInDay);
+                            $totalSeconds = ($totalTimeInDay - $expressionQuantity) * $secondsInDay;
+                            echo $expressionQuantity . "unit of " . $quantity . "d___";
+                            $alreadyValidExpressionExtracted["d"] = true;
+                        } else {
+                            echo "0 unit of " . $quantity . "d___";
+                        }
+                        break;
+                    case "h" :
+                        if (($totalSeconds / ($secondsInHour * $quantity)) > 1) {
+                            $totalTimeInHour = $totalSeconds / ($secondsInHour * $quantity);
+                            $expressionQuantity = intval($totalTimeInHour);
+                            $totalSeconds = ($totalTimeInHour - $expressionQuantity) * $secondsInHour;
+                            echo $expressionQuantity . "unit of " . $quantity . "h___";
+                            $alreadyValidExpressionExtracted["h"] = true;
+                        } else {
+                            echo "0 unit of " . $quantity . "h___";
+                        }
+                        break;
+                    case "i" :
+                        if (($totalSeconds / ($secondsInMin * $quantity)) > 1) {
+                            $totalTimeInMin = $totalSeconds / ($secondsInMin * $quantity);
+                            $expressionQuantity = intval($totalTimeInMin);
+                            $totalSeconds = ($totalTimeInMin - $expressionQuantity) * $secondsInMin;
+                            echo $expressionQuantity . "unit of " . $quantity . "i___";
+                            $alreadyValidExpressionExtracted["i"] = true;
+                        } else {
+                            echo "0 unit of " . $quantity . "i___";
+                        }
+                        break;
+                    case "s" :
+                        if (($totalSeconds / ($secondsInSecond * $quantity)) > 1) {
+                            $totalTimeInSecond = $totalSeconds / ($secondsInSecond * $quantity);
+                            $expressionQuantity = intval($totalTimeInSecond);
+                            $totalSeconds = ($totalTimeInSecond - $expressionQuantity) * $secondsInSecond;
+                            echo $expressionQuantity . "unit of " . $quantity . "i___";
+                            $alreadyValidExpressionExtracted["i"] = true;
+                        } else {
+                            echo "0 unit of " . $quantity . "i___";
+                        }
+                        break;
+                }
+            }
+
 
             $count++;
         }
-
-
-
 
 
         return "breakTime from service is called";
@@ -84,7 +183,6 @@ function sortTimeStrings(array $timeStrings): array
     }
 
     return array_merge($result["m"], $result["d"], $result["h"], $result["i"], $result["s"]);
-
 
 
 }
