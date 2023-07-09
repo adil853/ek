3c9f83de291e3b54abd54cc57e5b7a7ec69a0fc7
Adil Abdullah <adilabdullah@LHR-Adil-Abdullah-MAC.local>
2023-07-09 16:53:54 +0500

    remove non arc code

--- app/Http/Controllers/TimeController.php
+++ app/Http/Controllers/TimeController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\UseCases;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\App;
 
 class TimeController extends Controller
 {
@@ -25,37 +24,7 @@ class TimeController extends Controller
             'end_time' => $request->input('end_time'),
             'time_expressions' => $request->input('time_expressions')
         ];
-
         return $this->timeUseCase->execute($inputs);
-    }
-
-
-
-
-
-
-    public function breakTimeOld(Request $request)
-    {
-        $inputs = [
-            'start_time' => $request->input('start_time'),
-            'end_time' => $request->input('end_time'),
-            'time_expressions' => $request->input('time_expressions')
-        ];
-
-        $validator = validateInput($inputs);
-        if ($validator->fails()) {
-            $errors = $validator->errors();
-            return response()->json(['errors' => $errors], 420);
-        }
-
-
-        $timeService = new \App\Services\TimeService();
-        $res = $timeService->breakTime($inputs);
-        if ($res["error"]) {
-            return response()->json(['errors' => $res["message"], 'sampleRequest' => ['start_time'=>'2020-01-01 00:00:00', 'end_time'=>'2020-03-15 00:00:10', 'time_expressions'=>["2m","1d", "2h", "3s"]]], 420);
-        }
-
-        return response()->json(["message"=>"Key in following json is unit name and value is its weightage","data" => $res["body"]],200);
     }
 
     public function searchBreakTime(Request $request)
@@ -64,50 +33,7 @@ class TimeController extends Controller
             'start_time' => $request->input('start_time'),
             'end_time' => $request->input('end_time'),
         ];
-
         return $this->searchBreakTime->execute($inputs);
-
-
-
-
-        $validator = validateSearchInput($inputs);
-        if ($validator->fails()) {
-            $errors = $validator->errors();
-            return response()->json(['errors' => $errors], 420);
-        }
-
-        $timeService = new \App\Services\TimeService();
-        return $timeService->getBreakTime($request->input('start_time'), $request->input('end_time'));
-
     }
-}
-
-
-function validateSearchInput(array $data)
-{
-    return \Illuminate\Support\Facades\Validator::make($data, [
-        'start_time' => 'required|date_format:Y-m-d H:i:s',
-        'end_time' => 'required|date_format:Y-m-d H:i:s',
-    ]);
-}
-function validateInput(array $data)
-{
-    return \Illuminate\Support\Facades\Validator::make($data, [
-        'start_time' => 'required|date_format:Y-m-d H:i:s',
-        'end_time' => 'required|date_format:Y-m-d H:i:s',
-        'time_expressions.*' => ['distinct'],
-        'time_expressions' => [
-            'required',
-            'array',
-            function ($attribute, $value, $fail) {
-                foreach ($value as $item) {
-                    if (!preg_match('/^(\d+)?[mdhis]$/', $item)) {
-                        $fail("The $attribute array can only contain strings with the formats 'm', 'd', 'h', 'i', 's' or positive integers followed by 'm', 'd', 'h', 'i', 's'.");
-                        break;
-                    }
-                }
-            },
-        ],
-    ]);
 }
 
